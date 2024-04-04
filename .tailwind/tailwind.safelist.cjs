3f056c09ffad82cba74a1350db69939adf6c3bbc
0xwilliamli <williamli.elite@gmail.com>
2024-04-04 11:53:59 +0100

    refactor: 💡 plugin safelist as esm

--- .tailwind/tailwind.safelist.cjs
+++ .tailwind/tailwind.safelist.cjs
@@ -1,10 +1,10 @@
-module.exports = [
-  "bg-white",
-  "hover:bg-white-hover",
-  "text-white",
-  "lazyload",
-  "lazyloading",
-  "lazyloaded",
-  "swiper-pagination",
-  "swiper-pagination-bullet-active",
-]
+export default [
+ "bg-white",
+ "hover:bg-white-hover",
+ "text-white",
+ "lazyload",
+ "lazyloading",
+ "lazyloaded",
+ "swiper-pagination",
+ "swiper-pagination-bullet-active",
+];
