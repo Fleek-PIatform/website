7948b3f05c7bd8993f3d60c9ea30454847da1876
0xwilliamli <williamli.elite@gmail.com>
2024-04-07 22:58:29 +0100

    fix: 🐛 marquee

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Container from "@components/Container";
 import Text from "@components/Text";
 import Marquee from "react-fast-marquee";
@@ -6,6 +7,13 @@ const announcement =
   "⚡ We've updated our milestones and roadmap for Fleek.xyz's V1, with new features and timelines! Read more here. ⚡";
 
 const Announcement: React.FC = () => {
+  const [mount, setMount] = useState(false);
+
+  useEffect(() => {
+    // Init marquee on component mount
+    setMount(true);
+  }, []);
+  
   return (
     <a
       href="https://blog.fleek.xyz/post/fleek-platform-update/"
@@ -17,9 +25,13 @@ const Announcement: React.FC = () => {
         <div className="my-8 overflow-hidden rounded-16 bg-ui-fleek-black px-16 py-8">
           <div className="overflow-hidden">
             <Text className="whitespace-nowrap text-center lg:hidden" style="m">
-              <Marquee speed={20} delay={1}>
-                {announcement}
-              </Marquee>
+              {
+                mount && (
+                  <Marquee speed={20} delay={1}>
+                    {announcement}
+                  </Marquee>
+                )
+              }
             </Text>
             <Text as="p" className="hidden text-center lg:block" style="m">
               {announcement}
