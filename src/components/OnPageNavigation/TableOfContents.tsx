84fd3f2228abb6627658eefd5127cb2dea8f15e4
0xwilliamli <williamli.elite@gmail.com>
2024-04-11 18:16:21 +0100

    refactor: 💡 add active on page title on scroll, and throttle the onpage

--- src/components/OnPageNavigation/TableOfContents.tsx
+++ src/components/OnPageNavigation/TableOfContents.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import type { FC } from 'react';
 import type { MarkdownHeading } from 'astro';
+import { throttle } from 'lodash-es';
 
 type ItemOffsets = {
 	id: string;
@@ -11,21 +12,33 @@ type Props = {
 	headings: MarkdownHeading[];
 };
 
+const THROTTLE_MS = 300;
+
 const TableOfContents: FC<Props> = ({
 	headings = [],
 }) => {
-	if (!headings.length) return;
-
 	const itemOffsets = useRef<ItemOffsets[]>([]);
 	const [activeId, setActiveId] = useState<string>('');
 
 	useEffect(() => {
-		setActiveId(window.location.hash.replace('#', ''));		
+		const doc = document.querySelector('.doc');
+		let activeHash = window.location.hash.replace('#', '');
+
+		if (!activeHash && !window.scrollY) {
+			const el = doc?.querySelector('h1, h2, h3, h4');
+			
+			if (!el) return;
+
+			activeHash = el.id;
+		}
+		
+		setActiveId(activeHash);
 	}, []);
 	
 	useEffect(() => {
 		const getItemOffsets = () => {
 			const titles = document.querySelectorAll('article :is(h1, h2, h3, h4)');
+
 			itemOffsets.current = Array.from(titles).map((title) => ({
 				id: title.id,
 				topOffset: title.getBoundingClientRect().top + window.scrollY,
@@ -40,10 +53,41 @@ const TableOfContents: FC<Props> = ({
 		};
 	}, []);
 
-  const onClickHandler = (activeId: string) => {
-  	console.log('onClick', activeId)
-    setActiveId(activeId);
-  };
+	useEffect(() => {
+		const doc = document.querySelector('.doc');
+		const update = throttle(() => {
+			const sections = doc.querySelectorAll('h1, h2, h3, h4');
+			let currentSectionId;
+      const isAtBottom = window.scrollY + window.innerHeight >= document.body.scrollHeight;
+
+			sections.forEach(section => {
+				const sectionTop = section.getBoundingClientRect().top;
+				if (sectionTop <= 0) {
+					currentSectionId = section.id;
+				}
+			});
+
+			if (!currentSectionId) {
+				currentSectionId = sections[0].id;
+			}
+
+			if (isAtBottom) {
+				currentSectionId = sections[sections.length-1].id;
+			}
+
+			setActiveId(currentSectionId);
+		}, THROTTLE_MS);
+
+		window.addEventListener('scroll', update);
+
+		return () => {
+			window.removeEventListener('scroll', update);
+		};
+	}, []);
+
+	const onClickHandler = (activeId: string) => {
+		setActiveId(activeId);
+	};
 
 	return (
 		<>
