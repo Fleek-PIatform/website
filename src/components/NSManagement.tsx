15a0c622bcec6f7a8397de0ee18c1e0a1aed98a4
0xwilliamli <williamli.elite@gmail.com>
2024-04-16 15:33:50 +0100

    refactor: 💡 import img domains

--- src/components/NSManagement.tsx
+++ src/components/NSManagement.tsx
@@ -1,25 +1,24 @@
 import ImageWithCopy from "@components/ImageWithCopy";
+// @ts-ignore
+import imgDnsEnsIpnsManagement from "@images/dns-ens-ipns-management.png?w=450&format=webp";
 
-import type { ReactNode } from "react";
-
-type Props = {
-  cta: string;
-  OptImgDnsEnsIpnsManagement?: ReactNode;
-}
-
-const NSManagement = ({ cta, OptImgDnsEnsIpnsManagement }: Props) => (
-    <ImageWithCopy
-      kicker="domains"
-      floatingImageEffect
-      rounded="top-small-bottom-big"
-      headline="Seamless DNS, ENS and IPNS Management"
-      copy="With Fleek you can easily hook up DNS & ENS domains to your apps, or use the CLI/SDK to programmatically assign IPNS names to any IPFS hashes or files."
-      cta={{ url: "https://fleek.xyz/domains/", text: cta }}
-    >
-      <div className="my-24 px-48 md:px-128 lg:my-0 lg:px-64 2xl:px-128">
-        { OptImgDnsEnsIpnsManagement }
-      </div>
-    </ImageWithCopy>
-  );
+const NSManagement = ({ cta }: { cta: string }) => (
+  <ImageWithCopy
+    kicker="domains"
+    floatingImageEffect
+    rounded="top-small-bottom-big"
+    headline="Seamless DNS, ENS and IPNS Management"
+    copy="With Fleek you can easily hook up DNS & ENS domains to your apps, or use the CLI/SDK to programmatically assign IPNS names to any IPFS hashes or files."
+    cta={{ url: "https://fleek.xyz/domains/", text: cta }}
+  >
+    <div className="my-24 px-48 md:px-128 lg:my-0 lg:px-64 2xl:px-128">
+      <img
+        className="object-contain mix-blend-screen"
+        src={imgDnsEnsIpnsManagement}
+        alt="DNS/ENS/IPNS Management"
+      />
+    </div>
+  </ImageWithCopy>
+);
 
 export default NSManagement;
