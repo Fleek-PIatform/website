f785f81f9d346a313d416dfcab098dedc68ebfcf
0xwilliamli <williamli.elite@gmail.com>
2024-04-16 15:29:48 +0100

    refactor: 💡 import img chain agnostic

--- src/components/ChainAgnostic.tsx
+++ src/components/ChainAgnostic.tsx
@@ -1,25 +1,27 @@
 import IconsCollectionWithHeadline from "@components/IconsCollectionWithHeadline";
 import settings from '@base/settings.json'
+// @ts-ignore
+import imgBolt from "@images/bolt.png?w=120&format=webp";
 
-import type { ReactNode } from 'react';
-
-type Props = {
-  optImgBolt?: ReactNode;
-}
-
-const ChainAgnostic = ({
-  optImgBolt
-}: Props) => {
+const ChainAgnostic = () => {
   return (
     <IconsCollectionWithHeadline
       headline="Everybody can be #OnFleek"
       subheadline={
         <div className="flex items-center justify-center gap-16">
-          {optImgBolt}
+          <img
+            src={imgBolt}
+            alt="fleek bolt icon"
+            className="h-32 w-16"
+          />
           <div className="typo-caption-m lg:typo-caption-l">
             chain agnostic
           </div>
-          {optImgBolt}
+          <img
+            src={imgBolt}
+            alt="fleek bolt icon"
+            className="h-32 w-16"
+          />
         </div>
       }
       items={settings.homepage.chainAgnostic.icons}
