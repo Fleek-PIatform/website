8e304f5b444179c4091436ac4155c20e89ef06bd
0xwilliamli <williamli.elite@gmail.com>
2024-04-19 20:38:11 +0100

    fix: 🐛 edit github url generation

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,7 @@
 import settings from "@base/settings.json";
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
 
 export const getSiteUrl = (): string => {
  const env = process.env.NODE_ENV || 'staging';
@@ -22,4 +25,56 @@ export const isActivePath = ({
   const re = new RegExp(`^\\/${basePath}(\\/[^\/]+)?$`);
 
   return re.test(lookup);
+}
+
+const fileExistsCaseInsensitive = ({
+  dirname,
+  filename,
+}: {
+  dirname: string;
+  filename: string;
+}) => {
+  try {
+    const filenames = fs.readdirSync(dirname);
+    const lowerCasefilenameName = filename.toLowerCase();
+    // return filenames.some(filename => filename.toLowerCase() === lowerCasefilenameName);
+
+    const data = filenames.reduce((acc, curr) => {
+      if (filename.toLowerCase() !== lowerCasefilenameName) return acc;
+
+      return dirname;
+    }, '');
+
+    console.log('data ->', data);
+
+    return filenames.some(filename => filename.toLowerCase() === lowerCasefilenameName);
+  } catch (error) {
+    return false;
+  }
+}
+
+export const getDocumentUrl = ({
+  pathname,
+}: {
+  pathname: string;
+}) => {
+  const currDir = fileURLToPath(import.meta.url);
+  const root = currDir.split('src')[0];
+
+  const dirname = path.join(root, 'src/content', pathname);
+  const split = pathname.split('/');
+  const slug = split[split.length - 1];
+
+  const fileExist = (filename: string) => fileExistsCaseInsensitive({
+    dirname,
+    filename,
+  });
+
+  const possibleFilenames = [slug, "index.md"];
+
+  const foundFile = possibleFilenames.find(file => fileExist(file));
+
+  if (!foundFile) throw Error('Oops! Could not determine the URL');
+
+  return `${pathname}/${foundFile}`;
 }
