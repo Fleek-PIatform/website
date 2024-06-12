7de999bf56d1139752e3163eb3446d5e056af964
0xwilliamli <williamli.elite@gmail.com>
2024-06-12 15:19:20 +0100

    chore: pulling

---
+++ src/components/Support/form/DynamicForm.tsx
@@ -0,0 +1,14 @@
+function DynamicForm() {
+  return (
+    <div>
+      <h1>Submit a request</h1>
+
+      <div>
+        <p>Please choose your issue below</p>
+        <form>{/* dropdown */}</form>
+      </div>
+    </div>
+  );
+}
+
+export default DynamicForm;
