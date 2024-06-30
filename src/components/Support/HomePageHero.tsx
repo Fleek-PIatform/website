7652003a273c391443a8d7a44f0c3b4e35c4b903
0xwilliamli <williamli.elite@gmail.com>
2024-06-30 04:25:42 +0100

    Update src/components/Support/HomePageHero.tsx
    Co-authored-by: Helder Oliveira <heldrida@gmail.com>

--- src/components/Support/HomePageHero.tsx
+++ src/components/Support/HomePageHero.tsx
@@ -5,7 +5,7 @@ import SupportSearch from './form/SupportSearch';
 function HomePageHero() {
   return (
     <PageSection rounded="all-big">
-      <div className="flex items-center justify-center ">
+      <div className="flex items-center justify-center">
         <div className="w-full py-[12rem]">
           <div className="mb-[3rem] text-center">
             <h1 className="text-[3rem] font-semibold xl:text-[3.4rem]">
