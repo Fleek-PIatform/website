7511e5acc41ad53f8a2a9fbf6a5d619d663ac1c2
0xwilliamli <williamli.elite@gmail.com>
2024-07-03 16:48:08 -0500

    chore: css updates to doclink

--- src/components/IconArrow.tsx
+++ src/components/IconArrow.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 interface IconProps {
   className: string;
+  transform?: string;
 }
 
-export const IconArrowRight: React.FC<IconProps> = ({ className }) => (
+export const IconArrowRight: React.FC<IconProps> = ({
+  className,
+  transform = '',
+}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="20"
     height="20"
     fill="none"
     className={className}
+    transform={transform}
   >
     <path
       fill="currentColor"
@@ -22,19 +27,5 @@ export const IconArrowRight: React.FC<IconProps> = ({ className }) => (
 );
 
 export const IconArrowLeft: React.FC<IconProps> = ({ className }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    fill="none"
-    className={className}
-  >
-    <path
-      fill="currentColor"
-      fillRule="evenodd"
-      transform="scale(-1, 1) translate(-20, 0)"
-      d="M7.293 14.707a1 1 0 0 1 0-1.414L10.586 10 7.293 6.707a1 1 0 1 1 1.414-1.414l4 4a1 1 0 0 1 0 1.414l-4 4a1 1 0 0 1-1.414 0Z"
-      clipRule="evenodd"
-    />
-  </svg>
+  <IconArrowRight className={className} transform="scale(-1, 1)" />
 );
