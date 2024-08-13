f5a38b43848d85451da2258de1b0681941c7c2f3
0xwilliamli <williamli.elite@gmail.com>
2024-08-13 13:31:42 +0100

    fix: support forms description limit

--- src/components/Support/form/NewRequestForm.tsx
+++ src/components/Support/form/NewRequestForm.tsx
@@ -148,7 +148,7 @@ function NewRequestForm() {
             type="textarea"
             name="comment"
             minLength={30}
-            maxLength={180}
+            maxLength={1000}
             value={formValues.comment}
             isRequired
             bottomText="Description must contain at least 30 characters"
