7dbc51e117da9dc9716acf8f2563f36c5923ca8a
0xwilliamli <williamli.elite@gmail.com>
2024-08-16 10:53:56 -0300

    refactor: remove pagination (#148)
    ## Why?
    This PR removes pagination, so we have a simple changelog listing page
    that displays the 10 latest entries.
    
    ## Contribution checklist?
    
    - [x] The commit messages are detailed
    - [x] The `build` command runs locally
    - [ ] Assets or static content are linked and stored in the project
    - [ ] Document filename is named after the slug
    - [ ] You've reviewed spelling using a grammar checker
    - [ ] For documentation, guides or references, you've tested the
    commands and steps
    - [ ] You've done enough research before writing
    
    ## Security checklist?
    
    - [ ] Sensitive data has been identified and is being protected properly
    - [ ] Injection has been prevented (parameterized queries, no eval or
    system calls)
    - [ ] The Components are escaping output (to prevent XSS)
    
    ## References?
    
    Optionally, provide references such as links
    
    ## Preview?
    
    Optionally, provide the preview url here

--- src/pages/api/changelogs.json.ts
+++
@@ -1,9 +0,0 @@
-import { getCollection } from 'astro:content';
-
-export async function GET() {
-  const changelogs = (await getCollection('changelog'))
-    .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
-    .slice(0, 10);
-
-  return new Response(JSON.stringify(changelogs));
-}
